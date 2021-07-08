85f9302cd7ea5013ec883b8855e356f560cc8283
adam-talbot <adam.talbot527@gmail.com>
2021-07-08 17:20:29 -0500

    started working on first exercise question

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -5,3 +5,5 @@
 #1. You have rented some movies for your kids: 
 # The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). 
 # If price for a movie per day is 3 dollars, how much will you have to pay?
+
+# create a function that take in a list or dictionary? of strings and days and produces the total amount paid
