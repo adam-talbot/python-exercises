a5bf817ec8410180ebbe97ab4d1c8313aa4099e6
adam-talbot <adam.talbot527@gmail.com>
2021-07-08 17:14:05 -0500

    updated first juypter nb and started data types and variable exercises script

---
+++ data_types_and_variables.py
@@ -0,0 +1,7 @@
+#### DATA TYPES AND VARIABLES EXERCISES ####
+
+#Write python code to describe the following scenarios
+
+#1. You have rented some movies for your kids: 
+# The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). 
+# If price for a movie per day is 3 dollars, how much will you have to pay?
