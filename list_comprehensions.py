93f74321d321bc0192b55dea0dbb7b9caf120a71
adam-talbot <adam.talbot527@gmail.com>
2021-07-09 16:16:51 -0500

    did some different methods to solve vowels problems not using embedded list comprehension

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[2]:
 
 
 # 17 list comprehension problems in python
@@ -14,7 +14,7 @@ print(fruits)
 print(numbers)
 
 
-# In[4]:
+# In[3]:
 
 
 # Example for loop solution to add 1 to each number in the list
@@ -25,7 +25,7 @@ for number in numbers:
 print(numbers_plus_one)
 
 
-# In[5]:
+# In[4]:
 
 
 # Example of using a list comprehension to create a list of the numbers plus one.
@@ -34,7 +34,7 @@ numbers_plus_one = [number + 1 for number in numbers]
 print(numbers_plus_one)
 
 
-# In[7]:
+# In[5]:
 
 
 # Example code that creates a list of all of the list of strings in fruits and uppercases every string
@@ -45,7 +45,7 @@ for fruit in fruits:
 print(output)
 
 
-# In[9]:
+# In[6]:
 
 
 # Exercise 1 - rewrite the above example code using list comprehension syntax. Make a variable named uppercased_fruits to hold the output of the list comprehension. Output should be ['MANGO', 'KIWI', etc...]
@@ -54,7 +54,7 @@ uppercased_fruits = [fruit.upper() for fruit in fruits]
 print(uppercased_fruits)
 
 
-# In[10]:
+# In[7]:
 
 
 # Exercise 2 - create a variable named capitalized_fruits and use list comprehension syntax to produce output like ['Mango', 'Kiwi', 'Strawberry', etc...]
@@ -63,22 +63,50 @@ capitalized_fruits = [fruit.capitalize() for fruit in fruits]
 print(capitalized_fruits)
 
 
-# In[13]:
+# In[12]:
 
 
 # Exercise 3 - Use a list comprehension to make a variable named fruits_with_more_than_two_vowels. Hint: You'll need a way to check if something is a vowel.
-fruits_with_more_than_two_vowels = [fruit for fruit in fruits if [fruit, len([x for x in fruit if x in 'aeiouAEIOU'])][1] >= 2]
+fruits_with_more_than_two_vowels = [fruit for fruit in fruits if [len([letter for letter in fruit if letter in 'aeiouAEIOU'])][0] >= 2]
 print(fruits_with_more_than_two_vowels)
-# Not sure I completely understand what the nested list comprehension is doing here
 
 
-# In[15]:
+# In[ ]:
+
+
+# Exercise 3v2
+# This allows me to access the fruit that I am counting vowels in as well as the number of vowels in that fruit name
+fruits_with_more_than_two_vowels = [fruit for fruit in fruits if [fruit, len([letter for letter in fruit if letter in 'aeiouAEIOU'])][1] >= 2]
+print(fruits_with_more_than_two_vowels)
+
+
+# In[24]:
+
+
+# Exercise 3v3
+# I want to do this one in an easier way
+
+fruits_with_more_than_two_vowels = [fruit for fruit in fruits if (fruit.count("a") + fruit.count("e") + fruit.count("i") + fruit.count("o") + fruit.count("u")) >= 2]
+print(fruits_with_more_than_two_vowels)
+
+
+# In[26]:
 
 
 # Exercise 4 - make a variable named fruits_with_only_two_vowels. The result should be ['mango', 'kiwi', 'strawberry']
 fruits_with_only_two_vowels = [fruit for fruit in fruits if [fruit, len([x for x in fruit if x in 'aeiouAEIOU'])][1] == 2]
 print(fruits_with_only_two_vowels)
 # Not sure I completely understand what the nested list comprehension is doing here
+
+
+# In[27]:
+
+
+# Exercise 4v2
+# I want to do this one in an easier way
+
+fruits_with_only_two_vowels = [fruit for fruit in fruits if (fruit.count("a") + fruit.count("e") + fruit.count("i") + fruit.count("o") + fruit.count("u")) == 2]
+print(fruits_with_only_two_vowels)
 
 
 # In[16]:
