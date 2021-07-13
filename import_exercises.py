af79610c33238608c7ac60b093546b3d28da9a39
adam-talbot <adam.talbot527@gmail.com>
2021-07-13 14:47:41 -0500

    started import exercise

---
+++ import_exercises.py
@@ -0,0 +1,7 @@
+##### IMPORT EXERCISES PART 1 #####
+
+#1b. Use from to import the calculate_tip function directly. Call this function with values you choose and print the result.
+
+from function_exercises import calculate_tip # import desired function directly instead of importing entire module (.py file)
+
+print(calculate_tip(0.3, 100)) # call the function that was just imported and print result to the console
